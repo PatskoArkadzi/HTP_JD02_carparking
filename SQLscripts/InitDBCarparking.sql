d0a9a6e81c84649088a62c880a2a8f440fe20d3a
PatskoArkadzi <arkpatsko@gmail.com>
2018-04-09 23:05:59 +0200

    add SQL scripts

---
+++ SQLscripts/InitDBCarparking.sql
@@ -0,0 +1,32 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.1.10-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Дамп структуры базы данных carparking
+DROP DATABASE IF EXISTS `carparking`;
+CREATE DATABASE IF NOT EXISTS `carparking` /*!40100 DEFAULT CHARACTER SET ascii */;
+USE `carparking`;
+
+-- Дамп структуры для таблица carparking.cars
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE IF NOT EXISTS `cars` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `brand` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- Экспортируемые данные не выделены.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ SQLscripts/InsertDBCarparking.sql
@@ -0,0 +1,6 @@
+INSERT INTO `cars` (`id`, `brand`, `model`) VALUES
+	(1, 'Mercedes', 'e8'),
+	(3, 'Audi', 's5'),
+	(4, 'BMW', 'x5');
+INSERT INTO `orders` (`id`, `userId`, `carId`) VALUES
+	(1, 1, 1);
