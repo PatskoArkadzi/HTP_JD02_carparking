8084bf0dd05c10df100dc9f2abe3896772f7d08b
PatskoArkadzi <arkpatsko@gmail.com>
2018-04-17 17:43:33 +0300

    sql scripts

--- SQLscripts/InitDBCarparking.sql
+++ SQLscripts/InitDBCarparking.sql
@@ -1,29 +1,44 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.1.10-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              9.5.0.5196
+-- --------------------------------------------------------
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Дамп структуры базы данных carparking
 DROP DATABASE IF EXISTS `carparking`;
 CREATE DATABASE IF NOT EXISTS `carparking` /*!40100 DEFAULT CHARACTER SET ascii */;
 USE `carparking`;
 
+-- Дамп структуры для таблица carparking.cars
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE IF NOT EXISTS `cars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brand` varchar(50) DEFAULT NULL,
   `model` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_cars_orders` FOREIGN KEY (`id`) REFERENCES `orders` (`carId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
 
+-- Экспортируемые данные не выделены.
+-- Дамп структуры для таблица carparking.orders
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL DEFAULT '0',
   `carId` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `carId` (`carId`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
 
+-- Экспортируемые данные не выделены.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- SQLscripts/InsertDBCarparking.sql
+++ SQLscripts/InsertDBCarparking.sql
@@ -1,45 +1,59 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.1.10-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              9.5.0.5196
+-- --------------------------------------------------------
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Дамп структуры базы данных carparking
 DROP DATABASE IF EXISTS `carparking`;
 CREATE DATABASE IF NOT EXISTS `carparking` /*!40100 DEFAULT CHARACTER SET ascii */;
 USE `carparking`;
 
+-- Дамп структуры для таблица carparking.cars
 DROP TABLE IF EXISTS `cars`;
 CREATE TABLE IF NOT EXISTS `cars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brand` varchar(50) DEFAULT NULL,
   `model` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_cars_orders` FOREIGN KEY (`id`) REFERENCES `orders` (`carId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
 
+-- Дамп данных таблицы carparking.cars: ~3 rows (приблизительно)
 DELETE FROM `cars`;
 /*!40000 ALTER TABLE `cars` DISABLE KEYS */;
 INSERT INTO `cars` (`id`, `brand`, `model`) VALUES
 	(1, 'Mercedes', 'e8'),
 	(3, 'Audi', 's5'),
-	(4, 'BMW', 'x5'),
-	(5, 'bbb', '1111');
+	(4, 'BMW', 'x5');
 /*!40000 ALTER TABLE `cars` ENABLE KEYS */;
 
+-- Дамп структуры для таблица carparking.orders
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL DEFAULT '0',
   `carId` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `carId` (`carId`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=ascii ROW_FORMAT=COMPACT;
 
+-- Дамп данных таблицы carparking.orders: ~4 rows (приблизительно)
 DELETE FROM `orders`;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
 INSERT INTO `orders` (`id`, `userId`, `carId`) VALUES
-	(8, 1, 1),
-	(9, 1, 3),
-	(10, 1, 4),
-	(11, 1, 4);
+	(16, 1, 1),
+	(17, 1, 3),
+	(18, 1, 4),
+	(19, 1, 8);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
