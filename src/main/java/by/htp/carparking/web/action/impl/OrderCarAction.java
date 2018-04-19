44a61457b3ce53e196e90a0a3140796a385c34f9
PatskoArkadzi <arkpatsko@gmail.com>
2018-04-19 12:20:49 +0300

    fix configure xml

--- src/main/java/by/htp/carparking/web/action/impl/OrderCarAction.java
+++ src/main/java/by/htp/carparking/web/action/impl/OrderCarAction.java
@@ -2,26 +2,18 @@ package by.htp.carparking.web.action.impl;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import by.htp.carparking.db.dao.impl.OrderDaoDataBaseImpl;
 import by.htp.carparking.domain.Car;
-import by.htp.carparking.domain.Order;
 import by.htp.carparking.domain.User;
 import by.htp.carparking.service.CarService;
 import by.htp.carparking.service.OrderService;
-import by.htp.carparking.service.ServiceFactory;
 import by.htp.carparking.service.UserService;
-import by.htp.carparking.service.impl.CarServiceImpl;
-import by.htp.carparking.service.impl.OrderServiceImpl;
-import by.htp.carparking.service.impl.UserServiceImpl;
 import by.htp.carparking.web.action.BaseAction;
 
 import static by.htp.carparking.web.util.WebConstantDeclaration.*;
 
 import java.sql.Date;
-import java.util.List;
 
 import static by.htp.carparking.web.util.HttpRequestParamValidator.*;
 import static by.htp.carparking.web.util.HttpRequestParamFormatter.*;
@@ -74,17 +66,19 @@ public class OrderCarAction implements BaseAction {
 
 			String dateStart = request.getParameter("start");
 			String dateEnd = request.getParameter("end");
-			if (dateStart != null && dateEnd != null && orderService.isCarFree(orderCar.getId(),
-					Date.valueOf(request.getParameter("start")), Date.valueOf(request.getParameter("end")))) {
-				orderService.orderCar(formatString(userId), formatString(carId),
-						Date.valueOf(request.getParameter("start")), Date.valueOf(request.getParameter("end")));
-				
-				StringBuilder orderedCarMessage = new StringBuilder();
-				orderedCarMessage.append(orderCar.getBrand()).append(" ").append(orderCar.getModel())
-						.append(" was ordered succesfully");
-				request.setAttribute(REQUEST_PARAM_MSG_ORDER_CAR_SUCCESS, orderedCarMessage);
-			} else {
-				request.setAttribute(REQUEST_PARAM_MSG_ERROR, "В эти дни автомобиль занят");
+			if (dateStart != null && dateEnd != null) {
+				if (orderService.isCarFree(orderCar.getId(), Date.valueOf(request.getParameter("start")),
+						Date.valueOf(request.getParameter("end")))) {
+					orderService.orderCar(formatString(userId), formatString(carId),
+							Date.valueOf(request.getParameter("start")), Date.valueOf(request.getParameter("end")));
+
+					StringBuilder orderedCarMessage = new StringBuilder();
+					orderedCarMessage.append(orderCar.getBrand()).append(" ").append(orderCar.getModel())
+							.append(" was ordered succesfully");
+					request.setAttribute(REQUEST_PARAM_MSG_ORDER_CAR_SUCCESS, orderedCarMessage);
+				} else {
+					request.setAttribute(REQUEST_PARAM_MSG_ERROR, "В эти дни автомобиль занят");
+				}
 			}
 			return PAGE_USER_CAR_ORDER;
 		}
